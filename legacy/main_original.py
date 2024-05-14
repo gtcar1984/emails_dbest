ddde701bb58b3c875627ca0ee67c6149fdd231e3
Gabriel Carvalho <gtcar1984@gmail.com>
2024-05-14 17:28:49 -0300

    antes de modificar para plain text

--- legacy/main_original.py
+++ legacy/main_original.py
@@ -1,15 +1,15 @@
-import time
-start_time = time.time() # Pega a hora atual para medir o tempo de execução
-import os
-import csv
-import json
-import pathlib
-import smtplib
-from datetime import datetime
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from string import Template
 from dotenv import load_dotenv  # type: ignore
+from string import Template
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from datetime import datetime
+import smtplib
+import pathlib
+import json
+import csv
+import os
+import time
+start_time = time.time()  # Pega a hora atual para medir o tempo de execução
 
 # Carrega as informações de usuário e senha para login no SMTP do arquivo
 load_dotenv()
@@ -29,25 +29,26 @@ PATH_INFO = PATH_PARENT / 'info.json'
 with open(PATH_INFO, 'r') as info_file:
     info = json.load(info_file)
 
-POS = info.get('POS') # Pega a POSIÇÃO ATUAL NA SEQUÊNCIA
-MAX = info.get('MAX') # Pega o Limite para saber até onde ir
+POS = info.get('POS')  # Pega a POSIÇÃO ATUAL NA SEQUÊNCIA
+MAX = info.get('MAX')  # Pega o Limite para saber até onde ir
 
 
 # Verificação para saber se já chegou ao fim da sequência
 if POS < MAX:
 
     # Caminho arquivo HTML
-    PATH_HTML = PATH_SEQUENCE / info.get('PATHS')[POS] # Carrega o caminho na posição atual
+    # Carrega o caminho na posição atual
+    PATH_HTML = PATH_SEQUENCE / info.get('PATHS')[POS]
 
     # Configurações SMTP
     # Preciso dessas configs do HOSTINGER. GMAIL apenas para testes
-    smtp_server = os.getenv('SMTP_SERVER','') # 'smtp.gmail.com'
-    smtp_port = os.getenv('SMTP_PORT','')
+    smtp_server = os.getenv('SMTP_SERVER', '')  # 'smtp.gmail.com'
+    smtp_port = os.getenv('SMTP_PORT', '')
     smtp_username = os.getenv('EMAIL_USER', '')
     smtp_password = os.getenv('EMAIL_PASSWORD', '')
 
     # Abre o arquivo CSV e joga todos os dados na lista de dicionários para envio.
-    # Não é um problema agora e é mais rápido, mas aloca mais memória. 
+    # Não é um problema agora e é mais rápido, mas aloca mais memória.
     # DEVOPS - Verificar performance do servidor.
     with open(PATH_CSV, 'r') as file:
         DATA_LIST = [row for row in csv.DictReader(file)]
@@ -61,18 +62,19 @@ if POS < MAX:
                 text_file = file.read()
                 template = Template(text_file)
                 text_email = template.substitute(
-                    template_nome = dest['NOME'],
-                    template_empresa = dest['EMPRESA'],
-                    template_email = dest['EMAIL']
-                    )
+                    template_nome=dest['NOME'],
+                    template_empresa=dest['EMPRESA'],
+                    template_email=dest['EMAIL']
+                )
             # Assunto do email
-            subject = info.get('SUBJECTS')[POS] # Carrega o assunto na posição atual
-            template_subject = Template(subject) # Transforma em template
+            # Carrega o assunto na posição atual
+            subject = info.get('SUBJECTS')[POS]
+            template_subject = Template(subject)  # Transforma em template
             # Substitui o que quer que se queira do template para o campo no CSV
             subject_email = template_subject.substitute(
-                    template_nome = dest['NOME'],
-                    template_empresa = dest['EMPRESA'],
-                    template_email = dest['EMAIL']
+                template_nome=dest['NOME'],
+                template_empresa=dest['EMPRESA'],
+                template_email=dest['EMAIL']
             )
             # Monta o email
             mime_multipart = MIMEMultipart()
@@ -88,25 +90,28 @@ if POS < MAX:
                 server.starttls()
                 server.login(smtp_username, smtp_password)
                 server.send_message(mime_multipart)
-    
+
                 with open(PATH_LOG, 'a+', encoding='utf-8') as log:
-                    log.write((f'{datetime.strftime(datetime.now(), "%d/%m/%Y - %H:%M:%S")} - Email para {dest["NOME"]} da empresa {dest["EMPRESA"]} enviado com sucesso!\n'))
+                    log.write((f'{datetime.strftime(datetime.now(), "%d/%m/%Y - %H:%M:%S")} - Email para {
+                              dest["NOME"]} da empresa {dest["EMPRESA"]} enviado com sucesso!\n'))
 
         except smtplib.SMTPRecipientsRefused:
             with open(PATH_LOG, 'a', encoding='utf-8') as log:
-                log.write((f'{datetime.strftime(datetime.now(), "%d/%m/%Y - %H:%M:%S")} - Email para {dest["NOME"]} FALHOU!\n'))
+                log.write((f'{datetime.strftime(
+                    datetime.now(), "%d/%m/%Y - %H:%M:%S")} - Email para {dest["NOME"]} FALHOU!\n'))
 
-        
-    info['POS'] += 1 # Atualiza a posição na cadência de emails
+    info['POS'] += 1  # Atualiza a posição na cadência de emails
 
     # Aualiza o arquivo de configuração com a nova posição na cadência
     with open(PATH_INFO, 'w') as info_file:
-        json.dump(info, info_file, ensure_ascii= False, indent= 2)
+        json.dump(info, info_file, ensure_ascii=False, indent=2)
 
 else:
     with open(PATH_LOG, 'a', encoding='utf-8') as log:
-        log.write((f'{datetime.strftime(datetime.now(), "%d/%m/%Y - %H:%M:%S")} - Sequência chegou ao fim!\n'))
+        log.write((f'{datetime.strftime(datetime.now(),
+                  "%d/%m/%Y - %H:%M:%S")} - Sequência chegou ao fim!\n'))
 
 # Apenas para checar o tempo total de execução
 with open(PATH_LOG, 'a', encoding='utf-8') as log:
-    log.write(f'O tempo total de execução do main foi {time.time() - start_time}\n')
+    log.write(f'O tempo total de execução do main foi {
+              time.time() - start_time}\n')
--- main.py
+++ main.py
@@ -11,46 +11,61 @@ from email.mime.text import MIMEText
 from string import Template
 from dotenv import load_dotenv
 
-# Constants
+# Constants / Constantes
 PATH_PARENT = pathlib.Path(__file__).parent
 PATH_LOG = PATH_PARENT / 'log.txt'
 PATH_CSV = PATH_PARENT / 'data.csv'
 PATH_SEQUENCE = PATH_PARENT / 'cadencia'
 PATH_INFO = PATH_PARENT / 'info.json'
 PATH_ATTACHMENTS = PATH_PARENT / 'anexos'
-PATH_SIGNATURE = PATH_ATTACHMENTS / 'assinatura_andreza.JPG'
+PATH_SIGNATURE = PATH_ATTACHMENTS / 'assinatura_andreza.jpeg'
 
-# Load environment variables
+# Load environment variables / Carrega variaveis de ambiente
 load_dotenv()
 
 
 def load_info():
-    """Load information from info.json."""
+    """
+    Load information from info.json.
+    Carrega as informações do info.json
+    """
     with open(PATH_INFO, 'r') as info_file:
         return json.load(info_file)
 
 
 def save_info(info):
-    """Save information to info.json."""
+    """
+    Save information to info.json.
+    Atualiza info.json.
+    """
     with open(PATH_INFO, 'w') as info_file:
         json.dump(info, info_file, ensure_ascii=False, indent=2)
 
 
 def read_csv(filename):
-    """Read CSV file and return a generator of rows."""
+    """
+    Read CSV file and return a generator of rows.
+    Lê o arquivo CSV e retorna um gerador das linhas.
+    """
     with open(filename, 'r') as file:
         reader = csv.DictReader(file)
         yield from reader
 
 
 def load_email_template(template_path):
-    """Load email template from HTML file."""
+    """
+    Load email template from HTML file.
+    Carrega o template do arquivo HTML
+    """
     with open(template_path, 'r') as file:
         return Template(file.read())
 
 
 def load_subject_template(info_path, pos):
-    """Load subject template from info.json."""
+    """
+    Load subject template from info.json.
+    Carrega o template do assunto do arquivo info.json.
+    """
     with open(info_path, 'r') as info_file:
         info = json.load(info_file)
         subject_template = info.get('SUBJECTS')[pos]
@@ -64,18 +79,23 @@ def send_email(
     body, recipient,
     signature_path,
 ):
-    """Send email using SMTP server."""
+    """
+    Send email using SMTP server.
+    Envia o email usando SMTP.
+    """
     msg = MIMEMultipart()
     msg['From'] = alias
     msg['To'] = recipient
     msg['Subject'] = subject
 
     # Read signature image and encode it in base64
+    # Lê a imagem da assinatura e codifica em base64
     with open(signature_path, 'rb') as f:
         signature_data = f.read()
         signature_base64 = base64.b64encode(signature_data).decode('utf-8')
 
     # Insert the base64 encoded image data into the HTML body
+    # Insere a imagem codificada em base64 no corpo do HTML
     body_with_signature = body.replace(
         '<!--SIGNATURE_BASE64-->', signature_base64)
 
@@ -89,29 +109,40 @@ def send_email(
 
 
 def log_message(message):
-    """Write message to log file."""
+    """
+    Write message to log file.
+    Escreve a mensagem no arquivo de log.
+    """
     with open(PATH_LOG, 'a', encoding='utf-8') as log_file:
         log_file.write(message + '\n')
 
 
 def main():
+    """
+    Execute all functions in the correct order.
+    Executa todas as funções na ordem correta.
+    """
     start_time = time.time()
     info = load_info()
 
     # Get current position and maximum limit from info.json
+    # Pega a posição atual e o limite máximo do arquivo info.json
     POS = info.get('POS')
     MAX = info.get('MAX')
 
     # Abort the program if the cadence is completed
+    # Interrompe o programa se a cadência já foi completa
     if POS >= MAX:
         log_message(f'{datetime.now().strftime(
             "%d/%m/%Y - %H:%M:%S")} - Sequência chegou ao fim!')
         os.abort()
 
     # Construct HTML template path
+    # Contrói o caminho correto para o template HTML
     PATH_HTML = PATH_SEQUENCE / info.get('PATHS')[POS]
 
     # Get SMTP server settings from environment variables
+    # Pega as configurações do srvidor SMTP das variáveis de ambiente
     smtp_server = os.getenv('SMTP_SERVER', '')
     smtp_port = os.getenv('SMTP_PORT', '')
     smtp_username = os.getenv('EMAIL_USER', '')
@@ -119,12 +150,15 @@ def main():
     alias = os.getenv('ALIAS', '')
 
     # Load email template
+    # Carrega o template do email
     email_template = load_email_template(PATH_HTML)
 
     # Iterate through CSV data and send emails
+    # Corre o arquivo CSV e manda os emails
     for dest in read_csv(PATH_CSV):
         try:
             # Substitute placeholders in email template
+            # Substitui os placeholders no template do email
             body = email_template.substitute(
                 template_nome=dest['NOME'],
                 template_empresa=dest['EMPRESA'],
@@ -132,6 +166,7 @@ def main():
             )
 
             # Substitute placeholders in subject template
+            # Substitui os placeholders no templade do assunto
             subject_template = load_subject_template(PATH_INFO, POS)
             subject = subject_template.substitute(
                 template_nome=dest['NOME'],
@@ -140,6 +175,7 @@ def main():
             )
 
             # Send email
+            # Envia o email
             send_email(
                 smtp_server,
                 int(smtp_port),
@@ -153,6 +189,7 @@ def main():
             )
 
             # Log success
+            # Escreve o sucesso no log
             log_message(
                 f'{
                     datetime.now().strftime("%d/%m/%Y - %H:%M:%S")
@@ -164,21 +201,27 @@ def main():
             )
 
         except Exception as e:
-            # Log other errors
+            # Log errors
+            # Escreve erros no log
             log_message(f'{datetime.now().strftime(
                 "%d/%m/%Y - %H:%M:%S")} - Email para {dest["NOME"]} FALHOU!: {e}')
 
     # Update position in cadence
+    # Atualiza a posição na cadência
     info['POS'] += 1
 
     # Update info.json
+    # Atualiza o arquivo info.json
     save_info(info)
 
     # Log total execution time
+    # Escreve o tempo total de execução no log
     log_message(
         f'O tempo total de execução foi {time.time() - start_time}'
     )
 
 
+# Check if its the main file and execute the main function
+# Verifica se é o arquivo principal e executa a função main
 if __name__ == "__main__":
     main()
